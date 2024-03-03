aea0a5c4f669332bc45ddb319bf025c5dece2d31
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-03-03 13:05:41 +0100

    DBStorage - Place

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -12,6 +13,10 @@ class City(BaseModel, Base):
         __tablename__ = "cities"
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship(
+            "Place", backref="cities", cascade="all, delete-orphan"
+        )
+
     else:  # file storage case
         name = ""
         state_id = ""
