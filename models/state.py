4bc6e837bde82452f0297abddd089670b65c5e0b
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-03-03 12:13:57 +0100

    DBStorage - User

--- models/state.py
+++ models/state.py
@@ -13,7 +13,7 @@ class State(BaseModel, Base):
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
         cities = relationship("City", cascade="all, delete", backref="states")
-    else:
+    else:  # file storage case
         name = ""
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
