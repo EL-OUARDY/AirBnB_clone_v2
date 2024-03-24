f00f04f6382a0511532057aaaff509a793c6d7ec
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-03-24 15:03:15 -0700

    FIX: double quotes conflict

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,7 +21,7 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def c_lang(text: str):
     """Return C letter followed by passed parameter"""
-    return f"C {text.replace("_", " ")}"
+    return f'C {text.replace("_", " ")}'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
