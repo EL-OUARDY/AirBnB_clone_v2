0ca3cd7355fa4063bb14f12a3c96c29bf0596250
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-03-24 22:21:39 +0000

    Flask - catch integer parameter from the request route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def greet():
+    """Return a greet string"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Return HBNB string"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_lang(text: str):
+    """Return C letter followed by passed parameter"""
+    return f'C {text.replace("_", " ")}'
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_is_cool(text: str = "is cool"):
+    """Return “Python” string followed by passed parameter/default"""
+    return f'Python {text.replace("_", " ")}'
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def is_a_number(n: int):
+    """Display n is a number” only if n is an integer"""
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
