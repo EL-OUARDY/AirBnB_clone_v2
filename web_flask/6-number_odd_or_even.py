4ae6bfa7a3f19c35e6c8e22fbcdfd26ca0fad4c6
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-03-24 16:59:08 -0700

    Hot FIX

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -42,10 +42,12 @@ def number_template(n: int):
     """Display a HTML page only if n is an integer"""
     return render_template("5-number.html", number=n)
 
+
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n: int):
     """Display a HTML page only if n is an integer"""
     return render_template("6-number_odd_or_even.html", number=n)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
